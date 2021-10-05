020bb7c3a497d142d4a18aea396b0326308926c5
kruthikpavan <kruthikpavan24@gmail.com>
2021-10-05 19:09:38 +0530

    Add and search authors

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app= express()
 const expressLayouts= require('express-ejs-layouts')
-const indexRouter= require("./routes/index")
 require('dotenv').config()
 
 // pre-requisites
@@ -10,7 +9,8 @@ app.set('views',__dirname + '/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
-
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}));
 //mongoose
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true,useUnifiedTopology: true})
@@ -22,8 +22,11 @@ db.once('open',()=>console.log("database connected"))
 
 
 //routes
-app.use('/',indexRouter)
+const indexRouter= require("./routes/index")
+const authorRouter= require("./routes/authors")
 
+app.use('/',indexRouter)
+app.use('/authors',authorRouter)
 
 
 app.listen(process.env.PORT || 3000)
